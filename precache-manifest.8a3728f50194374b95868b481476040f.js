6ed2cdd007574fe8db962806185a1bee835b2f40
DESKTOP-GOA90O0\Theo Basallaje <theobasallaje@gmail.com>
2020-05-25 17:29:16 -0500

    Updates

---
+++ precache-manifest.8a3728f50194374b95868b481476040f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d9f0b20273ad8c60c766fe52c90d0c8",
+    "url": "/betterletter/index.html"
+  },
+  {
+    "revision": "08d45711c54eea14a551",
+    "url": "/betterletter/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "2dfd8ead6b8514b27d73",
+    "url": "/betterletter/static/js/2.18c7a0bc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/betterletter/static/js/2.18c7a0bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "08d45711c54eea14a551",
+    "url": "/betterletter/static/js/main.7137e295.chunk.js"
+  },
+  {
+    "revision": "b723c4c8a8a4a9b4b857",
+    "url": "/betterletter/static/js/runtime-main.e2732c54.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/betterletter/static/media/logo.ee7cd8ed.svg"
+  }
+]);
