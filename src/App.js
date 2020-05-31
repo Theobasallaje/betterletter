51573d5a7ba0f0b413c2b901ad434239c872837b
DESKTOP-GOA90O0\Theo Basallaje <theobasallaje@gmail.com>
2020-05-30 21:00:53 -0500

    added some code for the text editor

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+// import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
+// import { TextEditor } from "./components/TextEditor";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  componentDidMount() {
+  }
+
+  render() {
+    return (
+      <>
+      <h1>Welcome to Better letter...</h1>
+      {/* <Router> */}
+        {/* <Route path="/" exact component={TextEditor} /> */}
+      {/* </Router> */}
+      </>
+    );
+  }
 }
 
 export default App;
