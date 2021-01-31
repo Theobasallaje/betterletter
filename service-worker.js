f7aab4b01934c4aa0ba360a7763e709948b76dd4
Theobasallaje <theobasallaje@gmail.com>
2021-01-30 23:08:48 -0600

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c226be9be1ad544a2947dc520bf63fd7.js"
+  "/precache-manifest.47116aecb18d17471f74a257ba043e14.js"
 );
 
 self.addEventListener('message', (event) => {
