a52a2bbe6d03d09be19a569af61818122725b3d9
Theobasallaje <theobasallaje@gmail.com>
2022-09-10 18:58:38 -0500

    Adding link to privacy page

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,13 +1,22 @@
 import React, { useEffect } from "react";
 import { connect } from "react-redux";
-import { handleFabIcon, showShareButton, handleTextDetection } from "./../../actions";
+import {
+  handleFabIcon,
+  showShareButton,
+  handleTextDetection,
+} from "./../../actions";
 import Button from "@mui/material/Button";
 import "./About.scss";
 import "animate.css";
 import tangerineLogo from "./../../images/tdraft_logo.png";
 
 // function About(props) {
-function About({ handleFabIcon, showShareButton, handleTextDetection, hasText }) {
+function About({
+  handleFabIcon,
+  showShareButton,
+  handleTextDetection,
+  hasText,
+}) {
   useEffect(() => {
     handleFabIcon("back");
     showShareButton(false);
@@ -24,44 +33,36 @@ function About({ handleFabIcon, showShareButton, handleTextDetection, hasText })
             <li>Move your text to other apps</li>
             <li>Optimized for Mobile</li>
           </ul>
-          <h2 className="aboutHeader">Privacy</h2>
+          <h2 className="aboutHeader">About</h2>
           <p className="aboutParagraph">
-            tdraft.io does not collect
-            <i>
-              <b> any </b>
-            </i>
-            of your data. All of our code is open source and available
-            <a href="https://github.com/Theobasallaje/betterletter" target="_blank" rel="noopener noreferrer"> here</a>
+            tdraft.io is a product of IJNA Design based out of Dallas, TX. You
+            can find the source code for this project
+            <a
+              href="https://github.com/Theobasallaje/betterletter"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              {" "}
+              here
+            </a>
+            , and you can find our privacy policy
+            <a href="/privacy" target="_blank" rel="noopener noreferrer">
+              {" "}
+              here
+            </a>
+            .
           </p>
           <h2 className="aboutHeader">Contact</h2>
           <p className="aboutParagraph">
-            tdraft.io is developed by IJNA. If you have any questions or
-            feedback, please reach out to us.
+            If you have any questions or feedback, please reach out to us using
+            the following email address:
           </p>
-          {/* <div>
-              <p className="aboutParagraph">
-                tdraft is a simple, mobile-optimized text editor for the web.
-              </p>
-              <p className="aboutParagraph">
-                Type distraction free and then move your text to other apps.
-              </p>
-            </div> */}
-          <a href="mailto:jbasallaje@gmail.com" target="_blank" rel="noopener noreferrer">
-            <Button
-              variant="outlined"
-              sx={{
-                color: "black",
-                textAlign: "left",
-                borderColor: "black",
-                marginTop: "32px",
-                marginBottom: "8px",
-                marginRight: "8px",
-                borderWidth: "2px",
-                "&:hover": { borderWidth: "2px", borderColor: "black", background: "#FDDAA5" },
-              }}
-            >
-              <span className="emailButton">Email</span>
-            </Button>
+          <a
+            href="mailto:jbasallaje@gmail.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            jbasallaje@gmail.com
           </a>
         </div>
       </div>
