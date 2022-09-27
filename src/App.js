4a136df46667aa5ffd73f0eaa3f0ae1d60618c86
Theobasallaje <theobasallaje@gmail.com>
2022-09-26 20:33:32 -0500

    Changing back to browser router

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { isMobile } from "react-device-detect";
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { connect } from "react-redux";
 import { detectMobile } from "./actions/index";
 import Home from "./components/pages/Home";
