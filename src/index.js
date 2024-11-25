0f8d01af5f498bba3684c4679941db5914b394ab
Theobasallaje <theobasallaje@gmail.com>
2024-11-25 02:57:39 -0600

    Updating npm packages and misc fixes

--- src/index.js
+++ src/index.js
@@ -1,29 +1,34 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
-// Redux Setup 
+import * as serviceWorkerRegistration from './serviceWorkerRegistration'; // Import registration logic
+
+// Redux Setup
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, compose } from 'redux';
-import reduxThunk from 'redux-thunk';
+import { thunk } from 'redux-thunk';  // Corrected import
+
 // Reducers
 import reducers from './reducers';
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.register();
+serviceWorkerRegistration.register();  // Updated to serviceWorkerRegistration
 
 // Redux Dev Tool
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-//Creating Centreal Store for State
+// Creating Central Store for State
 const store = createStore(
   reducers,
-  composeEnhancers(applyMiddleware(reduxThunk))
+  composeEnhancers(applyMiddleware(thunk))  // Corrected to use 'thunk'
 );
-ReactDOM.render(
+
+// React 18 root rendering
+const root = ReactDOM.createRoot(document.getElementById('root'));  // Create a root using createRoot
+root.render(  // Use root.render() instead of ReactDOM.render()
   <Provider store={store}>
     <App />
   </Provider>
-  , document.getElementById('root'));
+);
